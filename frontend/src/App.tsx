f52bf3477698908666e687b9f8258ec9c5b81bc1
Arpan Das <arpan62825@gmail.com>
2025-10-14 01:11:30 +0530

    chore: initial commit with essential dependencies and project files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+const App = () => {
+  return (
+    <div className="h-screen bg-neutral-200">
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+    </div>
+  );
+};
+
+export default App;
