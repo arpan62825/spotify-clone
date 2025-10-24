00e30f8b5facfdf7657ceae46b9ac9d9c328740d
Arpan Das <arpan62825@gmail.com>
2025-10-24 16:19:32 +0530

    chore: trying to bugfix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,17 @@
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
+// import { Button } from "@/components/ui/button";
+// import {
+//   SignedIn,
+//   SignedOut,
+//   SignInButton,
+//   UserButton,
+// } from "@clerk/clerk-react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="h-screen bg-neutral-200">
-      <header>
-        <SignedOut>
-          <SignInButton>
-            <Button>Sign In</Button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage/>} />
+    </Routes>
   );
 };
 
