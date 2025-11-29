eab573f1514d22b911f6e0b1995a2ca4806585e8
Arpan Das <arpan62825@gmail.com>
2025-11-29 23:53:40 +0530

    chore: add home page and initialize layout

--- frontend/src/components/ui/addUserToDb.tsx
+++ frontend/src/components/ui/addUserToDb.tsx
@@ -11,7 +11,7 @@ const AddUserToDb = () => {
   useEffect(() => {
     const syncUser = async () => {
       try {
-        if (!isLoaded || !user) return;
+        if (!isLoaded || !user) return
 
         const result = await axiosInstance.post("/auth/callback", {
           id: user.id,
--- frontend/src/pages/layout/LayoutPage.tsx
+++ frontend/src/pages/layout/LayoutPage.tsx
@@ -7,6 +7,7 @@ import {
 import { useState, useEffect } from "react";
 
 const LayoutPage = () => {
+  // console.log(window.innerWidth);
   const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
     const handleWidth = () => {
@@ -19,21 +20,24 @@ const LayoutPage = () => {
   return (
     <div className="h-[calc(100vh-64px)] bg-neutral-950 flex flex-col">
       <ResizablePanelGroup direction="horizontal">
+        {/* Side Bar */}
         <ResizablePanel
           defaultSize={20}
           minSize={width <= 618 ? 0 : 10}
           maxSize={30}
           className="flex justify-center items-center"
         >
-          <h1>Side-bar</h1>
+          <h1>Side Bar</h1>
         </ResizablePanel>
 
         <ResizableHandle className="w-1 bg-neutral-900 cursor-pointer" />
 
+        {/* Main Content */}
         <ResizablePanel defaultSize={60}>
           <Outlet />
         </ResizablePanel>
 
+        {/* Chat Bar */}
         <ResizableHandle className="w-1 bg-neutral-900 cursor-pointer" />
 
         <ResizablePanel
