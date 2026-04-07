ba354215e531ea6e8842fd710ee534582937aefd
Arpan Das <arpan62825@gmail.com>
2026-04-07 22:28:08 +0530

    fix: trying to fix path issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../../frontend/dist");
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.get("/:catchAll(.*)", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
