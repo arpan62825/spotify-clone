47000a4f0224b761cef0f0688b27ebfd93a7c69e
Arpan Das <arpan62825@gmail.com>
2026-04-07 23:03:55 +0530

    fix: trying to fix path issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,7 +47,7 @@ app.use("/api/user", userRoutes);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
+  const frontendPath = path.join(__dirname, "../frontend/dist");
   app.use(express.static(frontendPath));
 
   app.get(/.*/, (req, res) => {
